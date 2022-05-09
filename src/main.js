63668d1827e5db422634eec4c7b4ee5de013366e
Michael Phannavong <mphannav@ucsc.edu>
2022-05-09 16:39:24 -0700

    finishing comments

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+//Michael Phannavong
+//Ching Kei Yiu
+//Yan Yang
+//Game Title: Slug Rush
+//Completed: 5/9/22
+//Creative Tilt: We tried doing a forest theme with the main character being a slug. We added "jungle-like" copyright free music in hopes to match that theme of being chases or running
+//through the wild. We also implemented a fast fall control in hopes of letting the character manuvere better. We also implemented multiple jumps but decided that would make the game too easy 
+//so we opted out to just allowing one jump. The code was made in a way that it that settings could easily be adjusted if wanted to.
 let cursors;
 const SCALE = 0.5;
-
 
 let config = {
     type: Phaser.WEBGL,
